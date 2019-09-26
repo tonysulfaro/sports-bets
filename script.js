2e8b3d50ea450fc8364688b9b4b57b48ec819d81
tony <tony116523@gmail.com>
2019-09-25 21:28:37 -0400

    information shown only when logged in

--- script.js
+++ script.js
@@ -62,6 +62,9 @@ const Bets = function () {
                     if (USERINFO['authenticated']) {
                         console.log('authenticated');
 
+                        // show games
+                        showGames();
+
                         // show alert
                         $('#login-failure-alert').hide();
                         $('#login-success-alert').show();
@@ -92,16 +95,44 @@ const Bets = function () {
                 event.preventDefault();
                 console.log('signup time');
 
-                var login_form = document.getElementById('loginform');
-                login_form.innerHTML = '';
+                console.log('lets try to signup at the endpoint')
 
-                let current_user = `<span class="navbar-text light">
-                email@gmail.com</span>`;
-                let logout_button = `<button id="logout-button" class="btn btn-outline-success my-2 my-sm-0" type="submit"
-                    value="logout">Logout</button>`;
+                let login_url = 'http://localhost:5000/login';
 
-                login_form.insertAdjacentHTML('beforeend', current_user)
-                login_form.insertAdjacentHTML('beforeend', logout_button)
+                let form_username = document.getElementById('username-input').value;
+                let form_password = document.getElementById('password-input').value;
+
+                console.log(form_username, form_password);
+
+                let payload = {
+                    username: form_username,
+                    password: form_password,
+                    submit_type: "signup"
+                };
+
+                console.log('payload')
+                console.log(payload)
+
+                try {
+                    const response = await fetch(login_url, {
+                        method: 'POST', // or 'PUT'
+                        body: JSON.stringify(payload), // data can be `string` or {object}!
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    const json = await response.json();
+                    console.log('Success:', json);
+
+                    if (response.status === 200) {
+                        alert('new user creation successful')
+                    } else {
+                        alert('Error ' + json['Message'])
+                    }
+
+                } catch (error) {
+                    console.error('Error:', error);
+                }
             }
             // logout button clicked
             else if (event.srcElement.id == 'logout-button') {
@@ -112,61 +143,9 @@ const Bets = function () {
             }
             // show games on nav click
             else if (event.srcElement.id == 'games-link') {
-
-                let url = 'https://api.collegefootballdata.com/games?year=2019&seasonType=regular&week=3';
-
-                fetch(url)
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then(function (json_response) {
-                        console.log(json_response);
-
-                        let game_container = document.getElementById('userView');
-                        game_container.innerHTML = "";
-
-                        //add game header
-                        let game_header = `<h1>Games</h1>`;
-                        game_container.insertAdjacentHTML('beforeend', game_header);
-
-                        json_response.forEach(element => {
-
-                            let home_team = element['home_team'];
-                            let away_team = element['away_team'];
-                            let start_date = element['start_date'];
-                            let home_points = element['home_points'];
-                            let away_points = element['away_points'];
-
-                            var gameCard = `
-                            <div class="card">
-                            <div class="card-header">
-                                ` + away_team + ' at ' + home_team + `
-                            </div>
-                            <div class="card-body">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-sm-9">
-
-                                            <p>Start Time: ` + start_date + `</p>
-                                            <h3>Current Score</h3>
-                                            <p>` + home_team + " : " + home_points + `</p>
-                                            <p>` + away_team + " : " + away_points + `</p>
-
-                                        </div>
-                                        <div class="col-sm-3">
-                                            <div class="card-actions">
-                                                <button type="button" class="btn btn-success">Place Bet</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>`;
-                            game_container.insertAdjacentHTML('beforeend', gameCard);
-
-                        });
-                    })
-
+                if (USERINFO['authenticated']) {
+                    showGames();
+                }
             }
             // show standings when authenticated
             else if (event.srcElement.id == 'standings-link') {
@@ -226,5 +205,61 @@ const Bets = function () {
                 standings_container.insertAdjacentHTML('beforeend', sample_table);
             }
         });
+    }
+
+    function showGames() {
+        let url = 'https://api.collegefootballdata.com/games?year=2019&seasonType=regular&week=3';
+
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json_response) {
+                console.log(json_response);
+
+                let game_container = document.getElementById('userView');
+                game_container.innerHTML = "";
+
+                //add game header
+                let game_header = `<h1>Games</h1>`;
+                game_container.insertAdjacentHTML('beforeend', game_header);
+
+                json_response.forEach(element => {
+
+                    let home_team = element['home_team'];
+                    let away_team = element['away_team'];
+                    let start_date = element['start_date'];
+                    let home_points = element['home_points'];
+                    let away_points = element['away_points'];
+
+                    var gameCard = `
+                            <div class="card">
+                            <div class="card-header">
+                                ` + away_team + ' at ' + home_team + `
+                            </div>
+                            <div class="card-body">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-sm-9">
+
+                                            <p>Start Time: ` + start_date + `</p>
+                                            <h3>Current Score</h3>
+                                            <p>` + home_team + " : " + home_points + `</p>
+                                            <p>` + away_team + " : " + away_points + `</p>
+
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <div class="card-actions">
+                                                <button type="button" class="btn btn-success">Place Bet</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>`;
+                    game_container.insertAdjacentHTML('beforeend', gameCard);
+
+                });
+            })
     }
 };
