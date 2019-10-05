13b8a3ef6976b215c1aa02bdaad6ed17a41876b6
toby <tony116523@gmail.com>
2019-10-04 21:39:10 -0400

    testing google sign in

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var SESSIONINFO = {
     authenticated: false,
+    googleAuthenticated: false,
     username: null,
     token: null,
     games: {
@@ -17,12 +18,21 @@ var SESSIONINFO = {
     }
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    SESSIONINFO.googleAuthenticated = true;
 
     // update global
     SESSIONINFO.authenticated = true;
@@ -481,6 +491,9 @@ const Bets = function () {
             // logout button clicked
             else if (event.srcElement.id == 'logout-button') {
                 event.preventDefault();
+                if (SESSIONINFO.googleAuthenticated) {
+                    signOut();
+                }
                 window.location.reload();
             }
             // show games on nav click
