b3e1e4c19e40c0a9a0aa3f9272136d1f33f0e371
toby <tony116523@gmail.com>
2019-10-04 21:22:29 -0400

    testing google sign in

--- script.js
+++ script.js
@@ -17,16 +17,16 @@ var SESSIONINFO = {
     }
 }
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 window.onload = function () {
     new Bets();
-
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    }
 }
 
 function getCookie(cname) {
